6668a00b5904c602fdf3897fc0f5a674fa7336db
Fiona Y. <fyeung21@gmail.com>
2025-04-07 23:45:54 -0700

    Add about page scaffolding

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,26 @@
 export default function About() {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section>
+      <h2>About Me</h2>
+      <article>
+        <div>
+          <img />
+        </div>
+        <div>
+          <p>
+            Location: <span>Vancouver, BC</span>
+          </p>
+          <section>biography here</section>
+        </div>
+      </article>
+      <article>
+        <h3>Skills</h3>
+        <ul>
+          <li>js</li>
+          <li>php</li>
+          <li>react</li>
+        </ul>
+      </article>
+    </section>
   );
 }
