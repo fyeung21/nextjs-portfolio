99d08ac609d488977e833162fdfe5622e9c19d64
Fiona Y. <fyeung21@gmail.com>
2025-04-08 23:50:06 -0700

    create project list

---
+++ public/projects.ts
@@ -0,0 +1,28 @@
+const projectsList = [
+  {
+    id: 0,
+    title: 'Catch Oscar',
+    brief: '',
+    img: '',
+    url: 'https://fiona-yeung.com',
+    github: 'https://github.com/fyeung21/catch-oscar',
+    platform: 'Web',
+    tags: ['HTML', 'CSS', 'JS'],
+    desc: [
+      {
+        id: 0,
+        title: 'Game Details & Features',
+        content: [
+          {
+            id: 0,
+            title: 'Game Details',
+            content:
+              '‘Catch Oscar’ is a game that has 9 holes where different neighbourhood cats could appear out of. Only one cat will show at one time and will disappear after a couple seconds.',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default projectsList;
